f1ec6d1245cdc4e04245d68b9cacbfc20f1a9f18
Fangxin Wang <2681086480@qq.com>
2020-10-10 17:02:34 +0800

    Juc

---
+++ src/main/java/com/kuang/tvolatile/VDemo2.java
@@ -0,0 +1,38 @@
+package com.kuang.tvolatile;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+// volatile 不保证原子性
+public class VDemo2 {
+
+    // volatile 不保证原子性
+    // 原子类的 Integer
+    private volatile static AtomicInteger num = new AtomicInteger();
+
+    public static void add(){
+        // num++; // 不是一个原子性操作
+        num.getAndIncrement(); // AtomicInteger + 1 方法， CAS
+    }
+
+    public static void main(String[] args) {
+
+        //理论上num结果应该为 2 万
+        for (int i = 1; i <= 20; i++) {
+            new Thread(()->{
+                for (int j = 0; j < 1000 ; j++) {
+                    add();
+                }
+            }).start();
+        }
+
+        // 线程数目大于2证明上面的线程还没有运行结束
+        // 因为两个默认线程是main gc
+        while (Thread.activeCount()>2){ // main  gc
+            Thread.yield();
+        }
+
+        System.out.println(Thread.currentThread().getName() + " " + num);
+
+
+    }
+}
